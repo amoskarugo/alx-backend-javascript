0c1623f32269e3870f0b002fcab53e1b7ca6e7dc
amoskarugo <karugoamos@gmail.com>
2024-04-10 21:14:00 +0300

    handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((res) => res)
-    .catch((err) => err)
-    .finally(console.log('Got a response from the API'));
+    .then((res) => console.log('Got a response from the API'))
+    .catch((err) => console.log('Got a response from the API'));
 }
