275a90d61253a3dcc065fbaed39d6590e08391bf
amoskarugo <karugoamos@gmail.com>
2024-04-10 22:07:54 +0300

    promise all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
-function handleProfileSignup() {
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
   const [promise1, promise2] = [uploadPhoto(), createUser()];
   Promise.all([promise1, promise2])
     .then(([result1, result2]) => {
-      console.log(
-        `${result1['body']} ${result2['firstName']} ${result2['lastName']}`
-      );
+      console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
