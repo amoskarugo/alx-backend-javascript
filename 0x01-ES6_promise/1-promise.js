dcbadf07ba9da056e3cc6b4a23428c2e84cb82ef
amoskarugo <karugoamos@gmail.com>
2024-04-10 17:17:25 +0300

    promise resolve&reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    success
-      ? resolve({ status: 200, body: "Success" })
-      : reject(Error("The fake API is not working currently"));
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
   });
 }
