917383bc2f6655b7b1b986066a7f4db30b4f6e57
amoskarugo <karugoamos@gmail.com>
2024-04-10 23:17:41 +0300

    promise all settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,11 +4,9 @@ import { signUpUser } from './4-user-promise';
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((res) =>
-    res.map((o) => ({
-      status: o.status,
-      value: o.status === 'fulfilled' ? o.value : String(o.reason),
-    }))
-  );
+    uploadPhoto(filename),
+  ]).then((res) => res.map((o) => ({
+    status: o.status,
+    value: o.status === 'fulfilled' ? o.value : String(o.reason),
+  })));
 }
