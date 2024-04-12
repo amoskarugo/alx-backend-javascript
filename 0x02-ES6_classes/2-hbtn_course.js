ad3987a1239627a67bdf0148be40b3d3bbae30a7
amoskarugo <karugoamos@gmail.com>
2024-04-12 13:24:37 +0300

    getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new Error('Name must be a string');
     if (typeof length !== 'number') throw new Error('Length must be a number');
-    if (!Array.isArray(students)) throw new Error('Students must be an  array');
+    if (!Array.isArray(students)) throw new Error('Students must be an array');
     this._name = name;
     this._length = length;
     this._students = students;
