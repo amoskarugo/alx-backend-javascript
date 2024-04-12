6580ae476759fe631b24d8a3886e443b2db049d4
amoskarugo <karugoamos@gmail.com>
2024-04-12 12:38:46 +0300

    array of class objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
 import { ClassRoom } from './0-classroom';
 
 export default function initializeRooms() {
-  const classObjects = [];
-  const values = [19, 20, 34];
-  for (const value of values) classObjects.push(new ClassRoom(value));
-
-  return classObjects;
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
