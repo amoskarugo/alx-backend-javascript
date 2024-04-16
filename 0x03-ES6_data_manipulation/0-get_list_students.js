8aa93ab7a55eb70b5aa380d832434c7cb10dcd25
amoskarugo <karugoamos@gmail.com>
2024-04-16 21:45:15 +0300

    array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-export default function getListStudents() {
+function getListStudents() {
   const students = [
     ['Guillaume', 1, 'San Francisco'],
     ['James', 2, 'Columbia'],
@@ -7,13 +7,15 @@ export default function getListStudents() {
 
   const studentObjects = [];
 
-  for (let i = 0; i < students.length; i++) {
-    const student = new Object();
-    student.firstName = students[i][0];
-    student.id = students[i][1];
-    student.location = students[i][2];
+  for (let i = 0; i < students.length; i += 1) {
+    const student = { firstName: '', id: '', location: '' };
+    [student.firstName, ,] = students[i];
+    [, student.id] = students[i];
+    [, , student.location] = students[i];
     studentObjects.push(student);
   }
 
   return studentObjects;
 }
+// export default getListStudents;
+console.log(getListStudents());
